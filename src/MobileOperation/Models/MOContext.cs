b002254c8dcc188c831d53231672119fb178ca1f
あまみや ゆうこ <1@1234.sh>
2016-01-19 19:07:03 +0800

    Modeling

---
+++ src/MobileOperation/Models/MOContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MobileOperation.Models
+{
+    public class MOContext : IdentityDbContext<User>
+    {
+        public DbSet<Flow> Flows { get; set; }
+        public DbSet<FlowExecutor> FlowExecutors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(e =>
+            {
+                e.HasIndex(x => x.PhoneNumber);
+                e.HasIndex(x => x.Position);
+                e.HasIndex(x => x.PositionLevel);
+                e.HasIndex(x => x.Department);
+            });
+
+            builder.Entity<Flow>(e =>
+            {
+                e.HasIndex(x => x.SpecificTime);
+                e.HasIndex(x => x.AlarmTimeSpan);
+                e.HasIndex(x => x.Type);
+            });
+
+            builder.Entity<FlowExecutor>(e =>
+            {
+                e.HasKey(x => new { x.FlowId, x.UserId });
+                e.HasIndex(x => x.Status);
+                e.HasIndex(x => x.ExecutedTime);
+                e.HasIndex(x => x.ExecutingTime);
+                e.HasIndex(x => x.ReadTime);
+            });
+        }
+    }
+}
