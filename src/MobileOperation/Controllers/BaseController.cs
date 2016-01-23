58e23b022088f71846e7b0716b8e14f9ff076df8
あまみや ゆうこ <1@1234.sh>
2016-01-23 14:32:15 +0800

    Infrastructures

---
+++ src/MobileOperation/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Mvc;
+using MobileOperation.Models;
+
+namespace MobileOperation.Controllers
+{
+    public class BaseController : BaseController<MOContext, User, string>
+    {
+    }
+}
